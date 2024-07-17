762b62f3f402d04ca495aa3c92a1d437f71213af
Sameer Adil <sameeraadil.x@gmail.com>
2024-07-17 13:35:24 +0500

    final commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../components/Home/Hero'
 import Trending from '../components/Home/Trending'
 import Browse from '../components/Home/Browse'
+import Creators from '../components/Home/Creators'
 
 const page = () => {
   return (
@@ -9,6 +10,8 @@ const page = () => {
        <Hero />
        <Trending />
        <Browse />
+       <Creators />
+
     </div>
   )
 }
--- src/components/Home/HeaderText.jsx
+++ src/components/Home/HeaderText.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const HeaderText = ({heading, detail}) => {
   return (
     <div>
-      <h3 className=" font-medium text-3xl">{heading}</h3>
+      <h3 className="font-bold text-4xl">{heading}</h3>
       <p className="text-white text-lg mb-10">
         {detail}
       </p>
