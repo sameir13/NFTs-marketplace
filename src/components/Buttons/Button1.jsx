d1d2cba9090f41a1b239bda48f39d6eb477f2b3e
Sameer Adil <sameeraadil.x@gmail.com>
2024-07-17 15:01:43 +0500

    navbar

--- src/components/Buttons/Button1.jsx
+++ src/components/Buttons/Button1.jsx
@@ -1,15 +1,10 @@
-import Link from 'next/link'
 import React from 'react'
 
-const Button1 = ({isLink, href}) => {
+const Button1 = ({children, marginBottomSm, marginTopSm, marginBottomMd, marginTopMd}) => {
   return (
-    <div>
-            {isLink ? (
-                <Link href={href}>{text}</Link>
-            ):(
-                <button onClick={callToAction}>{text}</button>
-            )}
-    </div>
+    <button className={`hidden md:flex w-full  justify-center  py-4 px-10 items-center gap-4 text-white  text-lg bg-[#a259ff] rounded-2xl md:w-fit ${marginBottomSm ? "mb-5" : marginTopSm ? "mt-5" : marginBottomMd ? "mb-7" : marginTopMd ? "mt-7" : null}`}>
+   {children}
+  </button>
   )
 }
 
